f169ebe06322a9fb482aefed180b46180e8d3f2e
Gaetan DURON <gduron@e1r7p2.42.fr>
2017-11-06 10:45:05 +0100

    change loginPage

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,9 +5,9 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = ({ startLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
-      <h1 className="box-layout__title">Boilerplate</h1>
-      <p>Tag line for app.</p>
-      <button className="button" onClick={startLogin}>Login with Google</button>
+      <h1 className="box-layout__title">Matcha</h1>
+	  <h3>Create Account</h3>
+      <button className="button" onClick={}>Create</button>
     </div>
   </div>
 );
