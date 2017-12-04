bcffabd3014227ba8c4668642ec3629a3d9115bc
Gaetan Duron <gaeduron@users.noreply.github.com>
2017-12-04 13:40:52 +0100

    Revert "Onboarding"

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,14 +4,10 @@ import createHistory from 'history/createBrowserHistory';
 import DashboardPage from '../components/DashboardPage';
 import NotFoundPage from '../components/NotFoundPage';
 import LoginPage from '../components/LoginPage';
-import Onboarding from '../components/onboarding/Onboarding';
 import PasswordResetPage from '../components/PasswordResetPage';
 import Notifications from '../components/Notification';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
-
-// dummy data
-import FBData from '../components/Onboarding/FBData.js'; 
 
 export const history = createHistory();
 
@@ -21,7 +17,6 @@ const AppRouter = () => (
 			<div>
 				<Notifications />
 				<Switch>
-					<PublicRoute path="/onboarding" component={() => <Onboarding FBData={FBData} />} exact={true} />
 					<PublicRoute path="/" component={LoginPage} exact={true} />
 					<PublicRoute path="/password-reset/:token" component={PasswordResetPage} />
 					<PrivateRoute path="/dashboard" component={DashboardPage} />
