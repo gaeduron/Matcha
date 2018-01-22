babf599450ddec8ef402b19a7488097fdef80548
Gaetan DURON <gduron@e3r1p2.42.fr>
2018-01-22 19:15:06 +0100

    User card need text hover animation removed

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
-import DashboardPage from '../components/DashboardPage';
+import DashboardPage from '../components/searchPage/page';
 import ProfilePage from '../components/ProfilePage';
 import ChatPage from '../components/ChatPage';
 import NewsPage from '../components/NewsPage';
