e1c95a962a8b526e6c4f7c0387871de515d38d5f
Gaetan DURON <gduron@e3r2p1.42.fr>
2018-01-29 14:11:39 +0100

    menu need tags and map

--- src/components/searchPage/menu.js
+++ src/components/searchPage/menu.js
@@ -5,30 +5,81 @@ import { Slider } from 'antd';
 export class SearchMenu extends React.Component {
 	constructor(props) {
 		super(props);
+		
+		this.state = {
+			distanceRange: [5, 40],	
+			ageRange: [24, 34],	
+			popularityRange: [50, 400],	
+		};
 	}
 
-	getValue = (value) => {
-		console.log(value);
+	getDistance = (value) => {
+		const distanceRange = value;
+		this.setState(() => ({ distanceRange }));
+	}
+	
+	getAge = (value) => {
+		const ageRange = value;
+		this.setState(() => ({ ageRange }));
+	}
+	
+	getPopularity = (value) => {
+		const popularityRange = value;
+		this.setState(() => ({ popularityRange }));
 	}
 
 	render() {
 		return (
 			<div className="">
-				<div>
-					<h2>Filters</h2>
-					<p>Sort by</p>
-					<select className="c-onb-form__input--select">
-						<option value="0">distance</option>
-						<option value="1">Age</option>
-						<option value="2">Popularity</option>
-					</select>
+				<div className="c-menu__title-container">
+					<h2 className="c-menu__title">Filters</h2>
+					<div>
+						<p className="c-sort__title">Sort by</p>
+						<select className="c-onb-form__input--select">
+							<option value="0">distance</option>
+							<option value="1">age</option>
+							<option value="2">popularity</option>
+						</select>
+					</div>
 				</div>
-				<div>
-					<p>Distance (km)</p>
-					<p>5</p>
-					<p>-</p>
-					<p>25</p>
-					<Slider range defaultValue={[20, 50]} disabled={false} onChange={this.getValue} />
+				<div className="c-slider">
+					<div className="c-slider__text-box">
+						<p className="c-slider__text c-slider__title">Distance (km)</p>
+						<div>
+							<p className="c-slider__text c-slider__value">{ this.state.distanceRange[0] }</p>
+							<p className="c-slider__text">-</p>
+							<p className="c-slider__text c-slider__value">
+								{ this.state.distanceRange[1] == 100 ? "100+" : this.state.distanceRange[1] }
+							</p>
+						</div>
+					</div>
+					<Slider range defaultValue={this.state.distanceRange} onChange={this.getDistance} />
+				</div>
+				<div className="c-slider">
+					<div className="c-slider__text-box">
+						<p className="c-slider__text c-slider__title">Age</p>
+						<div>
+							<p className="c-slider__text c-slider__value">{ this.state.ageRange[0] }</p>
+							<p className="c-slider__text">-</p>
+							<p className="c-slider__text c-slider__value">
+								{ this.state.ageRange[1] == 100 ? "100+" : this.state.ageRange[1] }
+							</p>
+						</div>
+					</div>
+					<Slider range min={18} defaultValue={this.state.ageRange} onChange={this.getAge} />
+				</div>
+				<div className="c-slider">
+					<div className="c-slider__text-box">
+						<p className="c-slider__text c-slider__title">Popularity</p>
+						<div>
+							<p className="c-slider__text c-slider__value">{ this.state.popularityRange[0] }</p>
+							<p className="c-slider__text">-</p>
+							<p className="c-slider__text c-slider__value">
+								{ this.state.popularityRange[1] == 1000 ? "1000+" : this.state.popularityRange[1] }
+							</p>
+						</div>
+					</div>
+					<Slider range max={1000} defaultValue={this.state.popularityRange} onChange={this.getPopularity} />
 				</div>
 			</div>
 		);
