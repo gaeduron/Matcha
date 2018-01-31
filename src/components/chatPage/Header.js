1626951f87506e120ad87af4431299fbe78293e3
gaetan duron <gaetan.duron@gmail.com>
2018-01-31 02:51:45 +0100

    started chat and profile page

---
+++ src/components/chatPage/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export class Header extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	onSort = () => this.props.showMenu();
+	onArrowBack = () => this.props.hideMenu();
+
+	render() {
+		if (this.props.menu === "hidden") {
+			return (
+				<header className="c-header">		
+					<h1 className="c-header--title">PROFILE</h1>
+					<i
+						className="material-icons c-header--icon-sort"
+						onClick={this.onSort}>more_vert
+					</i>
+				</header>
+			);
+		} else {
+			return (
+				<header className="c-header c-header__menu">	
+					<i
+						className="material-icons c-header--icon-arrow"
+						onClick={this.onArrowBack}>arrow_back
+					</i>
+				</header>
+			);
+		}
+	}
+}
+
+export default Header;
