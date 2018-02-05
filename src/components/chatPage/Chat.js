58f8bf3ae9aa284894f6e5d0c60056f6bff64657
gaetan duron <gaetan.duron@gmail.com>
2018-02-05 19:04:04 +0100

    messages are formated

--- src/components/chatPage/Chat.js
+++ src/components/chatPage/Chat.js
@@ -9,14 +9,49 @@ export class Chat extends React.Component {
 		super(props);
 
 		this.state = {
+			messages: [
+				{time: "18:34", from: "you", text: "Hey, how are you ?"},
+				{time: "18:34", from: "paola", text: "Fine and you?"},
+				{time: "18:34", from: "paola", text: "Do you come here often?"},
+				{time: "18:37", from: "you", text: "Fine thanks :), It's my first time using this app. Hopefully I will make some cool friends here !"},
+				{time: "18:37", from: "paola", text: "Me too, seem like the place too be ;)"},
+				{time: "18:40", from: "you", text: "Are you from paris, or are you traveling ?"},
+				{time: "18:40", from: "you", text: "I just arrived my self last week."},
+			],
 		};
+	}
+
+	changedWriter = (message, i, messages) => {
+		if (i !== 0 && message.from !== messages[i-1].from) {
+			return true;
+		}
+		return false;
+	}
+	
+	changedDay = (message, i, messages) => {
+		if (i === 0) {
+			return message.time;
+		} else if (message.time !== messages[i-1].time) {
+			return message.time;
+		}
+		return false;
 	}
 
 	render() {
 		return (
 			<div className="c-chat">
-				<p className="c-chat__date-text">20 DECEMBRE 2017, 18:34</p>
-				<Message text="On fait un test yo yo yoo yoooo "/>
+				<div className="c-chat__messages">
+				{this.state.messages.map((message, i, messages) => 
+						<Message
+							from={message.from}
+							time={message.time}
+							text={message.text}
+							changedWriter={this.changedWriter(message, i, messages)}
+							changedDay={this.changedDay(message, i, messages)}
+							key={i}
+						/>
+				)}
+				</div>
 				<div className="c-chat__message-box-wrapper">
 					<input className="c-chat__message-box" placeholder="Type a message..." />
 					<i className="material-icons c-chat__emoji">tag_faces</i>
--- src/components/chatPage/Message.js
+++ src/components/chatPage/Message.js
@@ -9,11 +9,40 @@ export class Message extends React.Component {
 		};
 	}
 
+	header = () => {
+		if (this.props.changedDay) {
+			return <p className="c-chat__date-text">${this.props.time}</p>;
+		} else if (this.props.changedWriter) {
+			<br />
+		}
+	}
+
 	render() {
 		return (
-			<div className="c-message__wrapper">
-				<div className="c-message">
-					<p>{this.props.text}</p>
+			<div>
+				{this.props.changedDay && 
+					<p className="c-chat__date-text">{this.props.changedDay}</p>
+				}
+				{this.props.changedWriter && !this.props.changedDay && 
+					 <br />
+				}
+				<div
+					className={`c-message__wrapper
+						${this.props.from == "you" ? "c-message__wrapper--sent" : ""}
+					`}
+				>
+					<div
+						className={`c-message
+							${this.props.from == "you" ? "c-message--sent" : ""}
+						`}
+					>
+						<p>{this.props.text}</p>
+						<p
+							className={`c-message__date 
+								${this.props.from == "you" ? "c-message__date--sent" : ""}
+							`}
+						>{this.props.time}</p>
+					</div>
 				</div>
 			</div>
 		);
