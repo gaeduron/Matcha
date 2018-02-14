2b1db83b7f3c92cdfe3cc62d919da745c2ba8c49
Benjamin DURON <bduron@e2r7p1.42.fr>
2018-02-14 19:27:23 +0100

    scraping photos and uploading them to cloudinary ok

--- server/postgresql/seed.js
+++ server/postgresql/seed.js
@@ -1,6 +1,136 @@
-var faker = require('faker');
-var randomLocation = require('random-location');
-var gender = require('gender');
+require('dotenv').config({ path: '../../.env.development' });
+const faker = require('faker');
+const randomLocation = require('random-location');
+const gender = require('gender');
+const fs = require('fs');
+const axios = require('axios');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+
+
+async function getCloudinaryPhotos(gender) {
+		
+	const url = `https://api.cloudinary.com/v1_1/matcha/resources/image/tags/${gender}`;
+
+	try {
+		const res = await axios.get(url, { 
+			auth: {
+				username: process.env.CLOUDINARY_API_KEY,
+				password: process.env.CLOUDINARY_API_SECRET
+			}		
+		});
+		const photos = res.data.resources;
+		return photos;
+	} catch(e) {
+		console.log(e);
+	}
+}
+
+
+function pickRand(arr) {
+	return arr[Math.floor(Math.random() * arr.length)];
+}
+
+
+function genUser(num) {
+
+	const orientation = ["bi", "gay", "hetero", "hetero", "hetero"];
+	const P = { latitude: 48.861014, longitude: 2.341155 }; // Paris center
+	const R = 1000 * 30; // 1km * xkm
+
+	const { latitude, longitude } = randomLocation.randomCirclePoint(P, R);
+	const identity = faker.helpers.contextualCard();
+	const guessedGender = gender.guess(identity.name).gender == 'male' ? 'man' : 'woman';
+
+
+	let user = {
+		score: Math.floor(Math.random() * 1000),
+		login: identity.username,
+		password: "$2a$10$yb/ec0CAOI4syhgGnFxXe.1DwSC97/93neXjL8ZVuY6qXJJwfmrJ.",
+		firstname: identity.name,
+		lastname: faker.name.lastName(),
+		email: identity.email,
+		sex: guessedGender,
+		sexualOrientation: pickRand(orientation),
+		bio: okcupid[Math.floor(Math.random() * okcupid.length)],
+		latitude,
+		longitude,
+		birthdate: faker.date.between('1985-01-01', '2000-01-01'),
+		photos: '[null, null, null, null ,null]', // TO DO 
+		geolocationAllowed: true,
+		occupation:	faker.name.jobTitle(),
+		onboarding: false
+	};
+
+	return user;
+}
+
+
+async function main() {
+
+	/* Launch concurrent async requests */
+	const okcupidPromise = readFile('okcupid', 'ascii');
+	const femalePromise = getCloudinaryPhotos('female');
+	const malePromise = getCloudinaryPhotos('male');
+
+	/* Catch all async returns */
+	const [ okcupidJson, female, male ] = await Promise.all([okcupidPromise, femalePromise, malePromise]);
+	const okcupid = JSON.parse(okcupidJson);
+//	console.log(okcupid);
+//	console.log(female);
+//	console.log(male);
+	
+	console.log(genUser());
+}
+
+
+main();
+
+
+//fs.readFile('okcupid', 'ascii', function(err, contents) {
+//
+//	let okcupid = JSON.parse(contents);
+//	
+//
+//	function genUser(num) {
+//
+//		const orientation = ["bi", "gay", "hetero", "hetero", "hetero"];
+//		const P = { latitude: 48.861014, longitude: 2.341155 }; // Paris center
+//		const R = 1000 * 30; // 1km * xkm
+//
+//		const { latitude, longitude } = randomLocation.randomCirclePoint(P, R);
+//		const identity = faker.helpers.contextualCard();
+//		const guessedGender = gender.guess(identity.name).gender == 'male' ? 'man' : 'woman';
+//
+//
+//		let user = {
+//			score: Math.floor(Math.random() * 1000),
+//			login: identity.username,
+//			password: "$2a$10$yb/ec0CAOI4syhgGnFxXe.1DwSC97/93neXjL8ZVuY6qXJJwfmrJ.",
+//			firstname: identity.name,
+//			lastname: faker.name.lastName(),
+//			email: identity.email,
+//			sex: guessedGender,
+//			sexualOrientation: pickRand(orientation),
+//			bio: okcupid[Math.floor(Math.random() * okcupid.length)],
+//			latitude,
+//			longitude,
+//			birthdate: faker.date.between('1985-01-01', '2000-01-01'),
+//			photos: '[null, null, null, null ,null]', // TO DO 
+//			geolocationAllowed: true,
+//			occupation:	faker.name.jobTitle(),
+//			onboarding: false
+//		};
+//
+//		return user;
+//	}
+//
+////	for (let i = 0; i < 3; i++)
+////		console.log(`user ${i}: \n`,genUser(i).bio);
+//
+//});
+
 
 
 const query = `
@@ -21,112 +151,70 @@ const query = `
 			geolocation_allowed,
 			occupation,
 			onboarding
-		) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16);
+	) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16);
 `;
 
-function pickRand(arr) {
-	return arr[Math.floor(Math.random() * arr.length)];
-}
-
-function genUser() {
-
-	const orientation = ["bi", "gay", "hetero", "hetero", "hetero"];
-	const P = { latitude: 48.861014, longitude: 2.341155 }; // Paris center
-	const R = 1000 * 30; // 1km * xkm
-
-	const { latitude, longitude } = randomLocation.randomCirclePoint(P, R);
-	const identity = faker.helpers.contextualCard();
-	const guessedGender = gender.guess(identity.name).gender == 'male' ? 'man' : 'woman';
-
-
-	let user = {
-		score: Math.floor(Math.random() * 1000),
-		login: identity.username,
-		password: "$2a$10$yb/ec0CAOI4syhgGnFxXe.1DwSC97/93neXjL8ZVuY6qXJJwfmrJ.",
-		firstname: identity.name,
-		lastname: faker.name.lastName(),
-		email: identity.email,
-		sex: guessedGender,
-		sexualOrientation: pickRand(orientation),
-		bio: faker.hacker.phrase(),
-		latitude,
-		longitude,
-		birthdate: faker.date.between('1985-01-01', '2000-01-01'),
-		photos: '[null, null, null, null ,null]', // TO DO 
-		geolocationAllowed: true,
-		occupation:	faker.name.jobTitle(),
-		onboarding: false
-	};
-
-	return user;
-}
-
-for (let i = 0; i < 3; i++)
-	console.log(`user ${i}: \n`,genUser());
-
-
-
 /*	
- 
+
 	1. Definir les champs du JSON template
 	2. Identifier les contraintes par champ
-		a. Localisation : generer des latitudes range[X - Y] et longitude range[A - B] random
-			perimetre ile de france 
+	a. Localisation : generer des latitudes range[X - Y] et longitude range[A - B] random
+	perimetre ile de france 
 
 	3. Generer 500 users avec faker
 	4. scrapper 500 x 2 urls de photos
-   		a. 2 array - 1st profile x500 / 2nd autre x500
+	a. 2 array - 1st profile x500 / 2nd autre x500
 	5. Construire requete SQL pour enregistrer 1 user 
 	6. Boucler sur les users json et lancer la requete SQL	
 
 
 
-   {
-	   id: 62, 
-	   		// N/A Auto increment by pgsql
-	   connected: null, 
-	   		// null
-	   score: 0, 
-	   		// rand [0 - 1000]
-	   reported: null, 
-	   		// null
-	   login: "narajaon",
-	   		// faker 
-	   password: "$2a$10$yb/ec0CAOI4syhgGnFxXe.1DwSC97/93neXjL8ZVuY6qXJJwfmrJ.", 
-	   		// Same for evryone 'Azerty123'
-	   firstname: "Fabien",
-	   		// faker 
-	   lastname: "Narajaon",
-	   		// faker 
-	   email: "narajaon@student.42.fr",
-	   		// faker 
-	   sex: "man",
-	   		// pickRand 
-	   sexualOrientation: "straight",
-	   		// pickRand
-	   bio: "Currently developing a patented way to produce noodle soups while POEing",
-	   		// faker 
-	   longitude: null,
-	   		// RandomLocation 
-	   latitude: null,
-	   		// RandomLocation 
-	   lastConnection: null,
-	   		// null 
-	   sessionToken: "e81a497b-9377-46e0-ba28-2465cfcf468e",
-	   		// null
-	   passwordResetToken: null,
-	   		// null
-	   passwordResetExpireAt: null,
-	   		// null
-	   birthdate: "1993-06-11T22:00:00.000Z",
-	   		// faker (format?)
-	   photos: "[\"https://res.cloudinary.com/matcha/image/upload/v1518520962/i6isiebcxslp3mclglbs.png\",null,null,null,null]",
-	   geolocationAllowed: false,
-	   		// true
-	   occupation: "Dev Swift",
-	   		// faker
-	   onboarding: true
-		   // false
-   }
+	{
+	id: 62, 
+// N/A Auto increment by pgsql
+connected: null, 
+// null
+score: 0, 
+// rand [0 - 1000]
+reported: null, 
+// null
+login: "narajaon",
+// faker 
+password: "$2a$10$yb/ec0CAOI4syhgGnFxXe.1DwSC97/93neXjL8ZVuY6qXJJwfmrJ.", 
+// Same for evryone 'Azerty123'
+firstname: "Fabien",
+// faker 
+lastname: "Narajaon",
+// faker 
+email: "narajaon@student.42.fr",
+// faker 
+sex: "man",
+// pickRand 
+sexualOrientation: "straight",
+// pickRand
+bio: "Currently developing a patented way to produce noodle soups while POEing",
+// faker 
+longitude: null,
+// RandomLocation 
+latitude: null,
+// RandomLocation 
+lastConnection: null,
+// null 
+sessionToken: "e81a497b-9377-46e0-ba28-2465cfcf468e",
+// null
+passwordResetToken: null,
+// null
+passwordResetExpireAt: null,
+// null
+birthdate: "1993-06-11T22:00:00.000Z",
+// faker (format?)
+photos: "[\"https://res.cloudinary.com/matcha/image/upload/v1518520962/i6isiebcxslp3mclglbs.png\",null,null,null,null]",
+geolocationAllowed: false,
+// true
+occupation: "Dev Swift",
+// faker
+onboarding: true
+// false
+}
 
- */
+*/
